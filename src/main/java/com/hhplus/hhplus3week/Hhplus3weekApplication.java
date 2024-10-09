ac73f68b6407f1dc418fee55736da22702cbe68b
Seulah Kim <seulahkim@chunjae.co.kr>
2024-10-09 15:35:14 +0900

    init commit

---
+++ src/main/java/com/hhplus/hhplus3week/Hhplus3weekApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplus3week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hhplus3weekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hhplus3weekApplication.class, args);
+    }
+
+}
