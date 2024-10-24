678e686d5c9acc2a51e89cee153552a381574f1f
seul <rser@hanmail.net>
2024-10-24 20:48:28 +0900

    create RestControllerAdvice

--- src/main/java/com/hhplus/hhplus3week/config/RestControllerAdvice.java
+++ src/main/java/com/hhplus/hhplus3week/config/RestControllerAdvice.java
@@ -4,13 +4,13 @@ import com.hhplus.hhplus3week.config.dto.ErrorCode;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.naming.AuthenticationException;
-import java.nio.file.AccessDeniedException;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
